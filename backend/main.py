4cf45af36c7d2664526b70d3d3790a6c69432c16
anveshaayushiworkspace-dotcom <anvesha.ayushiworkspace@gmail.com>
2025-12-30 04:54:29 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5174",
         "http://127.0.0.1:5174",
-        "https://aftermathh.vercel.app/",   
+        "https://aftermathh.vercel.app",   
     ],
     allow_credentials=True,
     allow_methods=["*"],
